fb2094cf9981fa6c7e7ca9802f73dd3b1f6987e4
Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>
2016-01-26 22:04:26 +0000

    Hold previous state of DStream so that the new DStream RDDs can be added to the RDDs in previous DStream to get a running total.

--- src/realtime_processing/kafka_stream.py
+++ src/realtime_processing/kafka_stream.py
@@ -13,6 +13,7 @@ from operator import add
 if __name__ == "__main__":
     sc = SparkContext(appName="rankmysteps")
     ssc = StreamingContext(sc, 1)
+    ssc.checkpoint("checkpoint")
 
     zkQuorum = "52.88.57.240:2181" 
     topic = "steps_data_part4"
@@ -36,6 +37,14 @@ if __name__ == "__main__":
             
             session.execute(prepared_write_query, (uuid, steps))
 
+    def updateFunc(new_value, last_state):
+        if last_state is None:
+            last_state = new_value
+            return last_state
+        else:
+            total_steps = add_steps(new_value,last_state)
+            return total_steps
+
     def add_steps(v1,v2):
         total_steps = v1[0] + v2[0]
         date1 = (datetime.datetime.strptime(v1[1], '%Y-%m-%d %H:%M:%S') -    
@@ -45,13 +54,19 @@ if __name__ == "__main__":
         latest_date = v1[1] if (date1 > date2) else v2[1]
         return (total_steps, latest_date)
 
-    def process(rdd):
-        users = rdd.map(lambda l: Row(uuid=json.loads(l)["uuid"],
-                          steps=json.loads(l)["steps"], timestamp=json.loads(l)["timestamp"]))
-        aggregated_steps = users.map(lambda u: (u[2],(u[0],u[1]))).reduceByKey(add_steps).collect()
-        for (uuid, (steps, date)) in aggregated_steps:
-             print("%s: %s %s"% (uuid, steps, date))
+    def print_users(rdd):
+	for (uuid, [(steps,time)]) in rdd.collect():
+                print("user: %s steps taken: %s time: %s"% (uuid, steps, time))
+
         
-    lines.foreachRDD(process)
+    running_count = lines.map(lambda l: Row(uuid=json.loads(l)["uuid"],
+                              steps=json.loads(l)["steps"], 
+                              timestamp=json.loads(l)["timestamp"]))\
+                         .map(lambda u: (u[2],(u[0],u[1])))\
+                         .reduceByKey(add_steps)\
+                         .updateStateByKey(updateFunc)
+
+    running_count.foreachRDD(print_users)
+
     ssc.start()
     ssc.awaitTermination()
