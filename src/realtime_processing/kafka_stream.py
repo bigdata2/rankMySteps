cea7c015fc2a875c9fcdd0b6033ff10f8292a590
Ubuntu <ubuntu@ip-172-31-0-133.us-west-2.compute.internal>
2016-02-06 21:58:47 +0000

    Disabled in-memory computation of total steps using spark. Faced out-of-memory issue, will revisit it later. For now, the old value is read from cassandra and added to the new steps. Also, not comparing and storing using H-M-S in a timestamp as cassandra schema  is now clustered using just the date field. Will revist in future if time is required.

--- src/realtime_processing/kafka_stream.py
+++ src/realtime_processing/kafka_stream.py
@@ -5,20 +5,28 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import SQLContext, Row
 from pyspark.sql.types import *
+from pyspark import SparkConf
 from cassandra.cluster import Cluster
 from cassandra import ConsistencyLevel
+from cassandra.query import named_tuple_factory
 import datetime
-from operator import add
+
+def getSqlContextInstance(sparkContext):
+    if ('sqlContextSingletonInstance' not in globals()):
+        globals()['sqlContextSingletonInstance'] = SQLContext(sparkContext)
+    return globals()['sqlContextSingletonInstance']
+
 
 if __name__ == "__main__":
 
-    sc = SparkContext(appName="rankmysteps")
-    ssc = StreamingContext(sc, 1)
-    ssc.checkpoint("checkpoint")
+    conf = SparkConf().setAppName("rankmysteps")
+    sc = SparkContext(conf=conf)
+    ssc = StreamingContext(sc, 2)
 
     zkQuorum = "52.88.57.240:2181" 
     topic = "steps_data_part4"
-    kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+    brokers = "52.88.57.240:9092"
+    kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
     lines = kvs.map(lambda x: x[1])
     
     def updateFunc(new_value, last_state):
@@ -26,41 +34,47 @@ if __name__ == "__main__":
             last_state = new_value
             return last_state
         else:
-            total_steps = add_steps(new_value,last_state)
-            return total_steps
-
-    def add_steps(v1,v2):
-        total_steps = v1[0] + v2[0]
-        date1 = (datetime.datetime.strptime(v1[1], '%Y-%m-%d %H:%M:%S') -    
-                 datetime.datetime(1970,1,1,0,0,0)).total_seconds()
-        date2 = (datetime.datetime.strptime(v2[1], '%Y-%m-%d %H:%M:%S') -    
-                 datetime.datetime(1970,1,1,0,0,0)).total_seconds()
-        latest_date = v1[1] if (date1 > date2) else v2[1]
-        return (total_steps, latest_date)
+            if new_value:
+                [(steps_new,dt_new)] = new_value
+                [(steps_old,dt_old)] = last_state
+                total_steps = steps_new + steps_old
+                return [(total_steps, dt_new)]
+            else:
+                return last_state
 
     def print_users(rdd):
-	for (uuid, [(steps,time)]) in rdd.collect():
+	for (uuid, (steps,time)) in rdd.collect():
                 print("user: %s steps taken: %s time: %s"% (uuid, steps, time))
 
     def write_into_cassandra(record):
         # connect to cassandra
-        cluster = Cluster(['ec2-52-35-237-159.us-west-2.compute.amazonaws.com'])
-        session = cluster.connect("ranksteps") 
-        prepared_write_query = session.prepare("INSERT INTO top_walkers "\
+        cluster = Cluster(['ec2-52-88-171-137.us-west-2.us-west-2.compute.amazonaws.com'])
+        session = cluster.connect("rank_steps") 
+        write_query_walker_steps = session.prepare("INSERT INTO walkers_steps"\
                                                "(user, num_steps, "\
                                                "arrival_time) VALUES (?,?,?)")
-	for (uuid, [(steps,time)]) in record:
-                print("user: %s steps taken: %s time: %s"% (uuid, steps, time))
+        read_query = session.prepare("SELECT user, num_steps FROM "\
+                                     "walkers_steps WHERE"\
+                                     " user = ? and arrival_time = ?")
+	for (uuid, (steps,time)) in record:
                 dt = datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
-                session.execute(prepared_write_query, (str(uuid), steps, dt))
-
+                rows = session.execute(read_query,(uuid,dt.strftime('%Y-%m-%d')))
+                if rows:
+                    userid = rows[0].user
+                    old_steps = int(rows[0].num_steps)
+                    total_steps = old_steps + steps
+                    session.execute(write_query_walker_steps, (uuid, total_steps,\
+                                    dt.strftime('%Y-%m-%d')))
+                else:
+                    session.execute(write_query_walker_steps, (uuid, steps,\
+                                    dt.strftime('%Y-%m-%d')))
         
     running_count = lines.map(lambda l: Row(uuid=json.loads(l)["uuid"],
                               steps=json.loads(l)["steps"], 
                               timestamp=json.loads(l)["timestamp"]))\
                          .map(lambda u: (u[2],(u[0],u[1])))\
-                         .reduceByKey(add_steps)\
-                         .updateStateByKey(updateFunc)
+                         .reduceByKey(lambda l1, l2: (l1[0]+l2[0], l2[1]))
+                         #.updateStateByKey(updateFunc)
 
     #running_count.foreachRDD(print_users) #for debugging 
     running_count.foreachRDD(lambda rdd: \
