f310d699700ea1fb9e7b849a5b05ea436e3dcc5b
Ubuntu <ubuntu@ip-172-31-0-132.us-west-2.compute.internal>
2016-02-06 22:34:35 +0000

    Modified producer to be asynchronous and send message in batches to increase throughput.

--- src/ingestion/kafka_producer.py
+++ src/ingestion/kafka_producer.py
@@ -12,26 +12,30 @@ class Producer(object):
 
     def __init__(self, addr):
         self.client = KafkaClient(addr)
-        self.producer = KeyedProducer(self.client)
+        self.producer = KeyedProducer(self.client,async=True,\
+                                      batch_send_every_n=500,batch_send=False)
         self.min_steps = 1
-        self.max_steps = 1000
-        self.max_users_each_thread = 2000000
+        self.max_steps = 3
+        self.max_users_each_thread = 12000
 
     def produce_msgs(self, source_symbol):
         msg_cnt = 0
         while True:
             start_uuid = (int(source_symbol) - 1) * self.max_users_each_thread
             stop_uuid =  (int(source_symbol) * self.max_users_each_thread) - 1
-            uuid = random.sample(range(start_uuid,stop_uuid), 300000)
+            uuid = random.sample(range(start_uuid,stop_uuid), 9)
             for uid in uuid:
-                timestamp = datetime.now(timezone('US/Pacific')).strftime('%Y-%m-%d %H:%M:%S')
-                steps = random.randint(1,1000)
+                timestamp = datetime.now(timezone('US/Pacific')).\
+                                        strftime('%Y-%m-%d %H:%M:%S')
+                steps = random.randint(1,10)
                 json_msg= {'source':source_symbol,'uuid':uid, 
                            'timestamp':timestamp, 'steps': steps}
                 json_encoded = json.dumps(json_msg)
-                self.producer.send_messages('steps_data_part4', source_symbol, json_encoded)
+                self.producer.send_messages('steps_data_part4', source_symbol,\
+                                             json_encoded)
                 print json_encoded
                 msg_cnt += 1
+                #sleep(2)
 
 if __name__ == "__main__":
     args = sys.argv
